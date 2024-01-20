08141713b4a64c9ca1367c3d753ffd44889f48ca
Rhys Adams <rhys@therhys.co.uk>
2024-01-20 16:57:15 +0000

    Put all functions in one file again.
    Also fix `CreateTeam` to put the owner in as a member.

--- functions.sql
+++ functions.sql
@@ -2,13 +2,24 @@ DROP FUNCTION IF EXISTS Inventory;
 DROP FUNCTION IF EXISTS TransferBalance;
 DROP FUNCTION IF EXISTS ApplyToBuy;
 DROP FUNCTION IF EXISTS ListPurchaseApplications;
-DROP FUNCTION IF EXISTS ApprovePurchaseApplication;
+DROP PROCEDURE IF EXISTS ApprovePurchaseApplication;
+
+DROP PROCEDURE IF EXISTS CreateTeam;
+DROP PROCEDURE IF EXISTS RequestJoinTeam;
+DROP FUNCTION IF EXISTS ListTeamRequests;
+DROP PROCEDURE IF EXISTS AcceptTeamRequest;
 
 -- Allows for SECURITY DEFINER functions to find the current user
 -- https://stackoverflow.com/questions/31712286/recording-the-invoker-of-a-postgres-function-that-is-set-to-security-definer
 DROP DOMAIN IF EXISTS whoami;
 CREATE DOMAIN whoami AS NAME
 CHECK ( VALUE = CURRENT_USER );
+
+
+
+-- User Functions --
+
+
 
 -- List all items in users inventory by name
 CREATE FUNCTION Inventory() RETURNS TABLE(name TEXT, price INTEGER) AS
@@ -82,7 +93,7 @@ END
 $func$ LANGUAGE PLPGSQL SECURITY INVOKER;
 
 -- Approve a purchase
-CREATE FUNCTION ApprovePurchaseApplication(applicationId INTEGER) RETURNS INTEGER AS
+CREATE PROCEDURE ApprovePurchaseApplication(applicationId INTEGER) AS
 $func$
 DECLARE
     applicationUser TEXT;
@@ -104,7 +115,7 @@ BEGIN
     IF alreadyApproved
     THEN
         RAISE NOTICE 'Application already approved';
-        RETURN 1;
+        RETURN;
     END IF;
 
     userBalance = userBalance - itemPrice;
@@ -112,7 +123,7 @@ BEGIN
     IF userBalance < 0
     THEN
         RAISE NOTICE 'Insufficient funds for approval';
-        RETURN 1;
+        RETURN;
     END IF;
 
     UPDATE "user" SET balance=userBalance
@@ -124,7 +135,76 @@ BEGIN
     INSERT INTO inventoryitem VALUES (applicationUser, itemId);
 
     RAISE NOTICE 'New balance: %, price %', userBalance, itemPrice;
-
-    RETURN 0;
 END
 $func$ LANGUAGE PLPGSQL SECURITY INVOKER;
+
+
+
+-- Team Functions --
+
+
+
+-- Create a team owned by you.
+CREATE PROCEDURE CreateTeam(teamName TEXT, owner whoami DEFAULT CURRENT_USER) AS
+$func$
+BEGIN
+    RAISE NOTICE 'Create team %. Leader %', teamName, owner;
+
+    INSERT INTO Team VALUES (teamName, owner);
+    INSERT INTO teammember VALUES (teamName, owner);
+end;
+$func$ LANGUAGE PLPGSQL SECURITY DEFINER;
+
+CREATE PROCEDURE RequestJoinTeam(teamToJoin TEXT, requestingUser whoami DEFAULT CURRENT_USER) AS
+$func$
+BEGIN
+    IF COUNT((SELECT 1 FROM teamjoinrequest
+                       WHERE username = requestingUser -- same user
+                       AND   teamname = teamToJoin     -- same team
+               )
+       ) > 0
+    THEN
+        RAISE NOTICE 'Request already sent or already in team';
+        RETURN;
+    END IF;
+
+    RAISE NOTICE 'Request to join team %.', teamToJoin;
+
+    INSERT INTO teamjoinrequest (teamname, username, approved) VALUES (teamToJoin, requestingUser, false);
+end;
+$func$ LANGUAGE PLPGSQL SECURITY DEFINER;
+
+CREATE FUNCTION ListTeamRequests() RETURNS TABLE(id INTEGER, teamName TEXT, requestingUser TEXT) AS
+$func$
+BEGIN
+    RETURN QUERY
+        SELECT teamjoinrequest.id, teamjoinrequest.teamname, username
+        FROM teamjoinrequest
+        WHERE approved <> true;
+end;
+$func$ LANGUAGE PLPGSQL SECURITY INVOKER;
+
+CREATE PROCEDURE AcceptTeamRequest(requestId INTEGER, callingUser whoami DEFAULT CURRENT_USER) AS
+$func$
+DECLARE
+    teamLeader TEXT;
+    teamName TEXT;
+    requestingUser TEXT;
+BEGIN
+    SELECT INTO teamLeader, teamName, requestingUser
+        team.leader, team.name, t.username
+        FROM team
+            JOIN public.teamjoinrequest t on team.name = t.teamname
+    WHERE t.id = requestId;
+
+    IF callingUser <> teamLeader
+    THEN
+        RAISE NOTICE 'You do not own this team';
+    END IF;
+
+    UPDATE teamjoinrequest SET approved=true
+    WHERE id = requestId;
+
+    INSERT INTO teammember VALUES (teamname, requestingUser);
+end;
+$func$ LANGUAGE PLPGSQL SECURITY DEFINER;
